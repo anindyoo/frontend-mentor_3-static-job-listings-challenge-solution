afd3d3d414f9a1a8f4a1302f18db3067c75a933d
anindyoo <anindyo0216@gmail.com>
2024-04-16 16:33:26 +0700

    feat: add header and main component.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import JobCard from "./components/job-card/JobCard";
+import JobData from "./data/data.json";
+
 function App() {
   return (
-    <div className="App">
-      
+    <div className="
+      App 
+      w-screen bg-lightGrayishCyanBackground 
+      text-darkGrayishCyan">
+      <header className="h-[9.75rem] bg-[url('./assets/images/bg-header-desktop.svg')] bg-desaturatedDarkCyan">
+      </header>
+      <main className="
+        flex flex-col items-center gap-6
+        px-8 pt-[4.75rem] pb-[6.063rem]"
+      >      
+        {JobData.map((data) => <JobCard data={data} key={`job-card-${data.id}`}/>)}
+      </main>
     </div>
   );
 }
