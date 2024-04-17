f420b982d8b22bb6cf003285f68b4102e17f18bf
anindyoo <anindyo0216@gmail.com>
2024-04-17 08:35:47 +0700

    feat: add JobCard component & filterCategory function.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,72 @@
+import { createContext } from "react";
 import JobCard from "./components/job-card/JobCard";
 import JobData from "./data/data.json";
+import { useState } from "react";
+
+export const FilterContext = createContext();
 
 function App() {
+  const [filterState, setFilterState] = useState({
+    role: '',
+    level: '',
+    languages: [],
+    tools: [],
+  });
+
+  const filterCategory = (name, value) => {
+    console.log("PASSING VALUES:", name, value)
+    if (name === 'languages' || name === 'tools') {
+      let pass = name === 'languages' ? !filterState.languages.includes(value) 
+        : name === 'tools' ? !filterState.tools.includes(value)
+        : true;
+      
+      pass && setFilterState((filter) => ({
+        ...filter,
+        [name]: [...filter[name], value],
+      }));
+    } else {
+      setFilterState((filter) => ({
+        ...filter,
+        [name]: value,
+      }));
+    }
+  };
+
   return (
     <div className="
       App 
       w-screen bg-lightGrayishCyanBackground 
       text-darkGrayishCyan">
-      <header className="h-[9.75rem] bg-[url('./assets/images/bg-header-desktop.svg')] bg-desaturatedDarkCyan">
+      <header className="
+        flex justify-center
+        h-[9.75rem] 
+        px-12
+        bg-[url('./assets/images/bg-header-desktop.svg')] bg-desaturatedDarkCyan"
+      >
+        <div className="
+          flex flex-row flex-wrap items-center
+          h-[4.5rem] w-full rounded-[0.25rem]
+          drop-shadow-lg-cyan bg-white 
+          px-10 py-5
+          mt-[7.5rem] 
+          absolute
+          xl:min-w-[69.375rem] xl:max-w-[69.375rem]"
+        >HELLO</div>
       </header>
       <main className="
         flex flex-col items-center gap-6
-        px-8 pt-[4.75rem] pb-[6.063rem]"
-      >      
-        {JobData.map((data) => <JobCard data={data} key={`job-card-${data.id}`}/>)}
+        px-12 pt-[4.75rem] pb-[6.063rem]"
+      >
+        <pre>
+        {JSON.stringify(filterState, undefined, 2)}
+
+        </pre>
+        {JobData.map((data) => 
+          <JobCard 
+            key={`job-card-${data.id}`}          
+            data={data} 
+            filterCategory={filterCategory}
+          />)}
       </main>
     </div>
   );
