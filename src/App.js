4440a8d4b781553cf99ff1e3d039f827f10afc92
anindyoo <anindyo0216@gmail.com>
2024-04-18 15:26:44 +0700

    feat: add jobDataState & function for removing category filter.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
 import JobCard from "./components/job-card/JobCard";
 import JobData from "./data/data.json";
 import { useState } from "react";
@@ -7,6 +7,7 @@ import CategoryFilterButton from "./components/category-filter-button/CategoryFi
 export const FilterContext = createContext();
 
 function App() {
+  const [jobDataState, setJobDataState] = useState(JobData);
   const [filterState, setFilterState] = useState({
     role: '',
     level: '',
@@ -14,7 +15,7 @@ function App() {
     tools: [],
   });
 
-  const filterCategory = (name, value) => {
+  const addCategoryFilter = (name, value) => {
     if (name === 'languages' || name === 'tools') {
       let pass = name === 'languages' ? !filterState.languages.includes(value) 
         : name === 'tools' ? !filterState.tools.includes(value)
@@ -32,19 +33,41 @@ function App() {
     }
   };
 
-  const filteredCategoryList = Object.values(filterState).filter(x => x.length > 0);  
+  const removeCategoryFilter = (name, value) => {
+    if (name === 'languages' || name === 'tools') {
+      setFilterState((filter) => ({
+        ...filter,
+        [name]: filter[name].filter((cat) => cat !== value),
+      }));
+    } else {
+      setFilterState((filter) => ({
+        ...filter,
+        [name]: '',
+      }));
+    }
+  }
 
-  JobData =  JobData.filter((job) => {        
-    return Object.entries(filterState)
-      .every(([key, value]) => {
-        const currentJobValue = job[key];
+  const filteredCategoryList = Object.entries(filterState)
+    .flatMap(([key, value]) => {
+      return value.length > 0 && (Array.isArray(value) ? value.map(v => [key, v]) : [[key,value]])
+    }
+  );
 
-        return !value.length ? true 
-          : Array.isArray(currentJobValue) 
-            ? value.every((element) => currentJobValue.includes(element)) 
-            : value === currentJobValue
+  useEffect(() => {
+     setJobDataState(JobData.filter((job) => {        
+      return Object.entries(filterState)
+        .every(([key, value]) => {
+          const currentJobValue = job[key];
+  
+          return !value.length ? true 
+            : Array.isArray(currentJobValue) 
+              ? value.every((element) => currentJobValue.includes(element)) 
+              : value === currentJobValue
+        })
       })
-  });
+    );
+  }, [filterState]);
+  
 
   return (
     <div className="
@@ -57,20 +80,22 @@ function App() {
         px-12
         bg-[url('./assets/images/bg-header-desktop.svg')] bg-desaturatedDarkCyan"
       >
-        {filteredCategoryList.length > 0 && (
+        {filteredCategoryList.some((cat) => cat) && (
           <div className="
-          animated animatedFadeInUp fadeInUp    
-            flex flex-row flex-wrap items-center
+            animated animatedFadeIn fadeInUp    
+            flex flex-row flex-wrap items-center gap-4
             h-[4.5rem] w-full rounded-[0.25rem]
             drop-shadow-lg-cyan bg-white 
-            px-10 py-5
+            px-10 
             mt-[7.5rem] 
-            absolute
+            z-10 absolute
             xl:min-w-[69.375rem] xl:max-w-[69.375rem]"
           >{filteredCategoryList.map((category, index) => (
-            <CategoryFilterButton
-              key={`category-filter-button-` +index}
-              category={category} 
+            category && <CategoryFilterButton
+              key={`category-filter-button-` + index}
+              categoryName={category[0]}
+              categoryValue={category[1]} 
+              removeCategoryFilter={removeCategoryFilter}
             />
           ))}</div>
         )}
@@ -79,15 +104,11 @@ function App() {
         flex flex-col items-center gap-6
         px-12 pt-[4.75rem] pb-[6.063rem]"
       >
-        <pre>
-        {JSON.stringify(filterState, undefined, 2)}
-
-        </pre>
-        {JobData.map((data) => 
+        {jobDataState.map((data) => 
           <JobCard 
             key={`job-card-${data.id}`}          
             data={data} 
-            filterCategory={filterCategory}
+            addCategoryFilter={addCategoryFilter}
           />)}
       </main>
     </div>
